7a3979390951bf298a39d47a0276a3c48864de6c
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-09-10 09:06:53 -0300

    Initial Commit

---
+++ ExemploWebView/PrincipalViewController.h
@@ -0,0 +1,13 @@
+//
+//  PrincipalViewController.h
+//  ExemploWebView
+//
+//  Created by Rafael Brigagão Paulino on 10/09/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PrincipalViewController : UIViewController
+
+@end
---
+++ ExemploWebViewTests/ExemploWebViewTests.m
@@ -0,0 +1,32 @@
+//
+//  ExemploWebViewTests.m
+//  ExemploWebViewTests
+//
+//  Created by Rafael Brigagão Paulino on 10/09/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import "ExemploWebViewTests.h"
+
+@implementation ExemploWebViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ExemploWebViewTests");
+}
+
+@end
